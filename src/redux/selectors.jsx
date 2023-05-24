3882a8f44cbd6d740555d767edd2ded21e2ba0db
Alexandr Katyshev <katyshev1@gmail.com>
2023-05-24 16:35:51 +0300

    adds new selector

--- src/redux/selectors.jsx
+++ src/redux/selectors.jsx
@@ -1,3 +1,14 @@
+import { createSelector } from '@reduxjs/toolkit';
+
 export const selectContacts = state => state.contacts.contactsList;
 export const selectFilter = state => state.filter.value;
 export const selectContactForm = state => state.contactForm.isFormOpened;
+
+export const selectFilteredContacts = createSelector(
+    [selectContacts, selectFilter],
+    (contacts, filter) => {
+        return contacts.filter(contact =>
+            contact.name.toLowerCase().includes(filter.toLowerCase())
+        );
+    }
+);
