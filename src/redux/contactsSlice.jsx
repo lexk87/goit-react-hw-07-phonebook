6fd0d2501ee2e996a68e5704a9fb6116f8c2ced0
Alexandr Katyshev <katyshev1@gmail.com>
2023-05-24 21:54:38 +0300

    adds changes to contacts slice

--- src/redux/contactsSlice.jsx
+++ src/redux/contactsSlice.jsx
@@ -1,19 +1,50 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, isAnyOf } from '@reduxjs/toolkit';
+import { fetchContacts, addContact, deleteContact } from './operations';
+
+const extraActions = [fetchContacts, addContact, deleteContact];
+
+const getExtraActions = type =>
+    isAnyOf(...extraActions.map(action => action[type]));
+
+const handleFetchContacts = (state, action) => {
+    state.contactsList = action.payload;
+};
+
+const handleAddContact = (state, action) => {
+    state.contactsList.unshift(action.payload);
+};
+
+const handleDeleteContact = (state, action) => {
+    state.contactsList = state.contactsList.filter(
+        ({ id }) => id !== action.payload
+    );
+};
+
+const handleFulfilledMatcher = state => {
+    state.isLoading = false;
+    state.error = null;
+};
+
+const handlePendingMatcher = state => {
+    state.isLoading = true;
+};
+
+const handleRejectedMatcher = (state, action) => {
+    state.isLoading = false;
+    state.error = action.payload;
+};
 
 const contactsSlice = createSlice({
     name: 'contscts',
     initialState: { contactsList: [], isLoading: false, error: null },
-    reducers: {
-        addContact(state, action) {
-            state.contactsList.unshift(action.payload);
-        },
-        deleteContact(state, action) {
-            state.contactsList = state.contactsList.filter(
-                ({ id }) => id !== action.payload
-            );
-        },
-    },
+    extraReducers: builder =>
+        builder
+            .addCase(fetchContacts.fulfilled, handleFetchContacts)
+            .addCase(addContact.fulfilled, handleAddContact)
+            .addCase(deleteContact.fulfilled, handleDeleteContact)
+            .addMatcher(getExtraActions('fulfilled'), handleFulfilledMatcher)
+            .addMatcher(getExtraActions('pending'), handlePendingMatcher)
+            .addMatcher(getExtraActions('rejected'), handleRejectedMatcher),
 });
 
-export const { addContact, deleteContact } = contactsSlice.actions;
 export const contactsReducer = contactsSlice.reducer;
